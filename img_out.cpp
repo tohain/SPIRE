60688a87e07e2403ad7892f1f240d8304d005240
Tobias Hain <tobiasmartin.hain@murdoch.edu.au>
2020-10-16 14:54:59 +0200

    - removed unnecessary and wrong code, since scaling moved to surface_projection class

--- img_out.cpp
+++ img_out.cpp
@@ -3,20 +3,6 @@
 
 
 void write_image( std::string fn, unsigned char *data, int width, int height, bool invert ){
-
-  //find max value
-  unsigned char min=std::numeric_limits<char>::max(), max=std::numeric_limits<char>::min();
-  for( unsigned int ii=0; ii<width*height; ii++){
-
-    if( data[ii] < min ){
-      min = data[ii];
-    }
-
-    if( data[ii] > max ){
-      max = data[ii];
-    }
-    
-  }
   
   //open filestream
   std::ofstream out ( fn );
@@ -39,13 +25,8 @@ void write_image( std::string fn, unsigned char *data, int width, int height, bo
 
       int ind = ii*width + jj;
       
-      //scale
-      int u_scaled = static_cast<int>((data[ind] - min)/(max - min)*255);
-      //invert if wanted
-      if( invert )
-	u_scaled = 255 - u_scaled;
-
-      out << u_scaled << " ";
+      
+      out << int(data[ind]) << " ";
     }
     out << std::endl;
   }  
